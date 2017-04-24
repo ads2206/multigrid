4cbf6df55e67cfd87908247c92f45a971749c69e
Avi <aks2203@columbia.edu>
2017-04-24 17:37:54 -0400

    error calculations not showing what I expected.

--- 2d.py
+++ 2d.py
@@ -90,66 +90,95 @@ def solve_jacobi(X, Y, alpha_x, beta_x, alpha_y, beta_y, f, iterations_J=None, U
 
 
 def main():
-    mg_iterations = 20
-    # delta_x = 1.0 / (m + 1)
-    # delta_y = 2.0 / (n + 1)
-    x = numpy.linspace(0.0, 1.0, m + 2)
-    y = numpy.linspace(0.0, 1.0, m + 2)
-    X, Y = numpy.meshgrid(x, y)
-    # Transpose these so that the coordinates match up to (i, j)
-    X = X.transpose()
-    Y = Y.transpose()    
-    u_true = lambda x, y: numpy.exp(x + y / 2.0)
+    mg_list = [1,2,4,10,20, 40, 80, 1000]
+    # mg_iterations = 2
+    for mg_iterations in mg_list:
+        # delta_x = 1.0 / (m + 1)
+        # delta_y = 2.0 / (n + 1)
+        x = numpy.linspace(0.0, 1.0, m + 2)
+        y = numpy.linspace(0.0, 1.0, m + 2)
+        X, Y = numpy.meshgrid(x, y)
+        # Transpose these so that the coordinates match up to (i, j)
+        X = X.transpose()
+        Y = Y.transpose()    
+        u_true = lambda x, y: numpy.exp(x + y / 2.0)
 
-        # Boundary Functions
-    alpha_x = lambda y: numpy.exp(y/2.0)
-    beta_x = lambda y: numpy.exp(1.0 + y/2.0)
-    alpha_y = lambda x: numpy.exp(x)
-    beta_y = lambda x: numpy.exp(x + 0.5)
-
-
-    
-    error_mg = []
-
-    U1, X1, Y1 = solve_jacobi(X, Y, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations)
-
-    U2, X2, Y2 = solve_jacobi(X[::2, ::2], Y[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U1.copy())
-    
-    U3, X3, Y3 = solve_jacobi(X2[::2, ::2], Y2[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U2.copy())
-    
-    U4, X4, Y4 = solve_jacobi(X3[::2, ::2], Y3[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U3.copy())
-    
-    U5, X5, Y5 = solve_jacobi(X3, Y3, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U4.copy())
-
-    U6, X6, Y6 = solve_jacobi(X2, Y2, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U5.copy())
-
-    U7, X7, Y7 = solve_jacobi(X1, Y1, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U6.copy())
+            # Boundary Functions
+        alpha_x = lambda y: numpy.exp(y/2.0)
+        beta_x = lambda y: numpy.exp(1.0 + y/2.0)
+        alpha_y = lambda x: numpy.exp(x)
+        beta_y = lambda x: numpy.exp(x + 0.5)
 
 
-    U_list = [U1 ,U2 ,U3 ,U4 ,U5 ,U6 ,U7]
-    X_list = [X1 ,X2 ,X3 ,X4 ,X5 ,X6 ,X7]
-    Y_list = [Y1 ,Y2 ,Y3 ,Y4 ,Y5 ,Y6 ,Y7]
+        
+        error_mg = []
 
-    plot_list = zip(U_list, X_list, Y_list)
-    for u, x, y in plot_list:
+        U1, X1, Y1 = solve_jacobi(X, Y, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations)
 
-        fig = plt.figure()
-        fig.set_figwidth(fig.get_figwidth())
-        axes = fig.add_subplot(1, 2, 1, aspect='equal')
-        plot = axes.pcolor(x, y, u, vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
-        fig.colorbar(plot, label="$U$")
-        axes.set_title("Computed Solution")
-        axes.set_xlabel("x")
-        axes.set_ylabel("y")
-        axes = fig.add_subplot(1, 2, 2, aspect='equal')
-        plot = axes.pcolor(x, y, u_true(x, y), vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
-        fig.colorbar(plot, label="$u(x,t)$")
-        axes.set_title("True Solution")
-        axes.set_xlabel("x")
-        axes.set_ylabel("y")
+        U2, X2, Y2 = solve_jacobi(X[::2, ::2], Y[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U1.copy())
+        
+        U3, X3, Y3 = solve_jacobi(X2[::2, ::2], Y2[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U2.copy())
+        
+        U4, X4, Y4 = solve_jacobi(X3[::2, ::2], Y3[::2, ::2], alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U3.copy())
+        
+        U5, X5, Y5 = solve_jacobi(X3, Y3, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U4.copy())
+
+        U6, X6, Y6 = solve_jacobi(X2, Y2, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U5.copy())
+
+        U7, X7, Y7 = solve_jacobi(X1, Y1, alpha_x, beta_x, alpha_y, beta_y, f, mg_iterations, U6.copy())
 
 
-    plt.show()
+
+        # U_list = [U1 ,U2 ,U3 ,U4 ,U5 ,U6 ,U7]
+        # X_list = [X1 ,X2 ,X3 ,X4 ,X5 ,X6 ,X7]
+        # Y_list = [Y1 ,Y2 ,Y3 ,Y4 ,Y5 ,Y6 ,Y7]
+
+        U_list = [U1, U7]
+        X_list = [X1, X7]
+        Y_list = [Y1, Y7]
+
+        plot_list = zip(U_list, X_list, Y_list)
+        # for u, x, y in plot_list:
+
+            # fig = plt.figure()
+            # fig.set_figwidth(fig.get_figwidth())
+            # axes = fig.add_subplot(1, 2, 1, aspect='equal')
+            # plot = axes.pcolor(x, y, u, vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
+            # fig.colorbar(plot, label="$U$")
+            # axes.set_title("Computed Solution")
+            # axes.set_xlabel("x")
+            # axes.set_ylabel("y")
+            # axes = fig.add_subplot(1, 2, 2, aspect='equal')
+            # plot = axes.pcolor(x, y, u_true(x, y), vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
+            # fig.colorbar(plot, label="$u(x,t)$")
+            # axes.set_title("True Solution")
+            # axes.set_xlabel("x")
+            # axes.set_ylabel("y")
+
+        U1, X1, Y1 = solve_jacobi(X, Y, alpha_x, beta_x, alpha_y, beta_y, f, 7*mg_iterations)
+
+        # fig = plt.figure()
+        # fig.set_figwidth(fig.get_figwidth())
+        # axes = fig.add_subplot(1, 2, 1, aspect='equal')
+        # plot = axes.pcolor(x, y, u, vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
+        # fig.colorbar(plot, label="$U$")
+        # axes.set_title("Computed Solution")
+        # axes.set_xlabel("x")
+        # axes.set_ylabel("y")
+        # axes = fig.add_subplot(1, 2, 2, aspect='equal')
+        # plot = axes.pcolor(x, y, u_true(x, y), vmax=7.0, vmin=0.0, cmap=plt.get_cmap("Blues"))
+        # fig.colorbar(plot, label="$u(x,t)$")
+        # axes.set_title("True Solution")
+        # axes.set_xlabel("x")
+        # axes.set_ylabel("y")
+
+        plt.show()
+
+        error = numpy.linalg.norm(U1 - u_true(X1, Y1), ord=2)
+        print error
+
+        error = numpy.linalg.norm(U7 - u_true(X7, Y7), ord=2)
+        print error
 
 if __name__ == "__main__":
     main()
