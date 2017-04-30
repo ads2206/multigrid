8aa28e2a0274143f18c25b8129d93fc98a779103
Andres Soto <ads2206@columbia.edu>
2017-04-30 18:06:10 -0400

    finish and test getMatrices.py - get_R and get_T functions

--- programs/getMatrices.py
+++ programs/getMatrices.py
@@ -2,85 +2,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.sparse as sparse
 
-def getInterpolation_T(m, level, max_level=4):
+def get_T(n):
 	'''
 	Parameters/Arguments
-	m: the number of discretizations within the interval [a,b]
+	n: the number of discretizations within the interval [a,b]
 	   including the boundary points
-	level: grid level
 
 	Output/Return
 	Returns the matrix interpolating matrix T that interpolates 
 	points from a grid level i to a grid level i+1 
 
-	fine to coarse discretization
-	
-	
-
+	coarse to fine interpolation
+	9 by 5
 	'''
-	if level >= max_level:
-		print("grid level must be less than the maximum grid level")
-		return
-	else:
-		T = np.zeros((m, m/2 + 2))
-		 #iterate row by row
-		j = k = 0
-		for i in range(T.shape[0]):
-			if (i % 2) == 0:
-				T[i, j] = 1
-				j += 1
-			else:
-				T[i, k] = .5
-				T[i, k+1] = .5
-				k+=1
-		print(T)
-		return T
+	T = np.zeros((2*n -1, n ))
+	 #iterate row by row
+	j = k = 0
+	for i in range(T.shape[0]):
+		if (i % 2) == 0:
+			T[i, j] = 1
+			j += 1
+		else:
+			T[i, k] = .5
+			T[i, k+1] = .5
+			k+=1
+	#T[0, :] = 0; T[0,0] = 1; T[-1,:] = 0; T[-1,-1] = 1
+	return T
 
-def getRestriction_R(m, level, max_level=4):
+def get_R(n):
 	''' 
 	Parameters/Arguments
-	m: the number of discretizations within the interval [a, b]
+	n: the number of discretizations within the interval [a, b]
 	   including the boundary points 
 	level: the grid level we are currently at
 
 	Output/Return
 	Returns the interpolating matrix R that interpolates a re-fining
-	of the discretization used at grid level i to a grid level at i-1
+	of the discretization used at grid level i to a grid level at i-1. 
+	The coarse grid vector (the output), takes its value directly from 
+	the corresponding fine grid point.
 
-	coarse to fine discretization
+	fine to coarse restriction
 	'''
-	if level <= 1: 
-		print("We must be at a level from 2 to the max level")
-		return 
-	else: 
-		R = np.zeros((m, 2*m-2))
-		# get corners
-		R[0,0] = 1
-		R[-1,-1] = 1
+	row = range(n)
+	col = [2*i for i in row]
 
-		j = 1
-		for i in range(R.shape[0]):
-			if i >= 1 and i < R.shape[0]-1:
-				R[i, j] = .25
-				R[i, j+1] = .5
-				R[i, j+2] = .25
-				j += 1
-		print R
-		return R
+	R = np.zeros((n, 2*n-1))
+	R[row, col] = 1
+	return R
+		# potential 
 
-# grid 1: 18 discretizations (16 interior + 2 boundary)
-# grid 2: 10 discretizations ( 8 interior + 2 boundary)
-# grid 3: 6 discretizations  ( 4 interior + 2 boundary)
-# grid 4: 4 discretizations  ( 4 interior + 2 boundary)
-
-
+# grid 1: 17 discretizations (15 interior + 2 boundary)
+# grid 2: 9 discretizations  ( 7 interior + 2 boundary)
+# grid 3: 5 discretizations  ( 3 interior + 2 boundary)
+# grid 4: 3 discretizations  ( 1 interior + 2 boundary)
 def main():
-	m = 9
-	T = getInterpolation_T(m, level=1)
-	R = getRestriction_R(m, level=2)
-	x = np.linspace(0, 1, 9)
-
-	print(x, np.dot(x, T))
+	n = 5
+	T = get_T(n)
+	R = get_R(n)
+	x = np.linspace(0, 1, 2*n-1)
+	print("input vector: ", x)
+	print("Restriction.    R dot x: ", np.dot(R, x))
+	x = np.linspace(0, 1, n)
+	print("input vector: ", x)
+	print("Interpolation.  T dot x: ", np.dot(T, x))
 
 if __name__ == "__main__":
 	main()
