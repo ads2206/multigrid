463867df9fb54244a3d5a22cfd48fd428323635d
Avi <aks2203@columbia.edu>
2017-04-30 18:21:52 -0400

    MultiGridClass.py mostly implemented for 1D.

--- MultiGridClass.py
+++ MultiGridClass.py
@@ -10,19 +10,22 @@
 
 ####
 # MultiGridClass
+# one dimensional for now.
 ####
 
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
-import scipy.sparse as sparse
+from getMatrices import get_T, get_R
 
 class MultiGridClass(self):
     ''' Class to manage iterative PDE solvers with 
         multigrid approach '''
 
-    def __init__(self, x, U0, domain, bc=(0.0,0.0), solver='jacobi'):
+    def __init__(self, x, U0, domain, f=0, bc=(0.0,0.0), solver='jacobi'):
         # array: grid descritization
         self.x = x
+        self.current_x = x
+        self.level = 1
         # self.y (to be added for 2d problems)
 
         # array: solution
@@ -37,23 +40,47 @@ class MultiGridClass(self):
         # String: iterative method name ex: 'jacobi'
         self.solver = solver
 
+        # function: non-homogenous term
+        self.f = f
+
     def restrict(self):
         ''' Fine to coarse grid using matrix R for projection '''
-        raise NotYetImplemented
+        R = get_R(len(self.x))
+        self.u = np.dot(R, self.u)
+        self.current_x = self.current_x[::2]
+        self.level += 1
+
 
     def interpolate(self):
         ''' Coarse to fine grid using matrix T for interpolation '''
-        raise NotYetImplemented
+        self.level -= 1
+        T = get_T(len(self.x))
+        self.u = np.dot(T, self.u)
+        jump = 2 ** (self.level - 1)
+        self.current_x = self.x[::jump]
 
     def iterative_solver(self, num_times=1):
         ''' Execute the interative solver to improve the solution u
         on current grid '''
-        raise NotYetImplemented
+        x_bc = self.current_x
+        x = x_bc[1:-1]
+        dx = x[1] - x[0]
+        m  = len(x)
+        for j in range(num_times):
+            for i in range(1, m + 1):
+                self.u[i] = 0.5 * (self.u[i+1] + self.u[i-1]) - f(x_bc[i]) * dx**2 / 2.0
+
 
     def plot(self):
         ''' Plot u(x) '''
-        raise NotYetImplemented
-
+        fig = plt.figure()
+        axes = fig.add_subplot(1, 1, 1)
+        axes.plot(self.current_x, self.u)
+            axes.set_title("Title needed")
+        axes.set_xlabel("x")
+        axes.set_ylabel("u(x)")
+        axes.legend(loc=2)
+        plt.show()
 
 
 
