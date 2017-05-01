9c56b563e6b372e9149ccdffa4606651937a9bd9
Avi <aks2203@columbia.edu>
2017-05-01 17:25:27 -0400

    MultiGrid2D.py has defect equation but is not executing properly. Looking into linear system.

--- programs/MultiGrid2D.py
+++ programs/MultiGrid2D.py
@@ -16,18 +16,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 from getMatrices import get_T, get_R
 import scipy.sparse as sparse
+
 class MultiGrid2D:
     ''' Class to manage iterative PDE solvers with 
         multigrid approach '''
 
-    def __init__(self, x, y, U0, domain, f, bc, u_true, solver='jacobi'):
+    def __init__(self, x, y, U0, domain, f, bc, u_true, name, solver='jacobi'):
         # array: grid descritization
+        self.name = name
         self.x = x
         self.y = y
         self.current_x = x
         self.current_y = y
         # manually insert maximum level (coarsest mesh)
-        self.max_levels = 4
+        self.max_levels = 3
+
 
         self.level = 1
         # self.y (to be added for 2d problems)
@@ -57,8 +60,32 @@ class MultiGrid2D:
         self.counter = 0    
         self.u_true = u_true
 
+        ### NEEDS WORK FOR RELAXING ERROR
+        # Some matrices to keep around:
+        # self.max_levels = np.log2(len(x)-1)
+        # assert(self.max_levels % 1 == 0)
+        # self.max_levels = int(self.max_levels)
+        self.A_list = []
+        # Store matrix A
+        mat_size = len(x)**2
+        e = np.ones(mat_size)
+        A = sparse.spdiags([e, -2*e, e], [-1,0,1], mat_size, mat_size).toarray()
+        A /= (x[1] - x[0])**2 
+        self.A_list.append(A)
+        # print A
+        for i in xrange(self.max_levels):
+            A = self.A_list[-1]
+            self.A_list.append(np.dot(get_R(int(np.sqrt(len(A)))/2 + 1, 2), np.dot(A, get_T(int(np.sqrt(len(A))) /2  + 1, 2))))
+
+    def set_boundary(self):
+        self.u[0, :] = self.bc[2](self.current_x)
+        self.u[-1, :] = self.bc[3](self.current_x)
+        self.u[:, 0] = self.bc[0](self.current_y)
+        self.u[:, -1] = self.bc[1](self.current_y)
+
     def restrict(self):
         ''' Fine to coarse grid using matrix R for projection '''
+        print 'number of points in x direction', len(self.current_x)
         R = get_R(len(self.current_x[::2]), 2)
         self.u = np.dot(R, self.u.reshape(len(self.u)**2))
         self.current_x = self.current_x[::2]
@@ -66,10 +93,12 @@ class MultiGrid2D:
         self.level += 1
         square = int(np.sqrt(len(self.u)))
         self.u = self.u.reshape((square,square))
-        self.u[0, :] = self.bc[2](self.current_x)
-        self.u[-1, :] = self.bc[3](self.current_x)
-        self.u[:, 0] = self.bc[0](self.current_y)
-        self.u[:, -1] = self.bc[1](self.current_y)
+        self.set_boundary()
+        # self.u[0, :] = self.bc[2](self.current_x)
+        # self.u[-1, :] = self.bc[3](self.current_x)
+        # self.u[:, 0] = self.bc[0](self.current_y)
+        # self.u[:, -1] = self.bc[1](self.current_y)
+        print 'number of points in x direction', len(self.current_x)
 
     def interpolate(self):
         ''' Coarse to fine grid using matrix T for interpolation '''
@@ -81,10 +110,12 @@ class MultiGrid2D:
         self.current_y = self.y[::jump]
         square = int(np.sqrt(len(self.u)))
         self.u = self.u.reshape((square,square))
-        self.u[0, :] = self.bc[2](self.current_x)
-        self.u[-1, :] = self.bc[3](self.current_x)
-        self.u[:, 0] = self.bc[0](self.current_y)
-        self.u[:, -1] = self.bc[1](self.current_y)
+        self.set_boundary()
+
+        # self.u[0, :] = self.bc[2](self.current_x)
+        # self.u[-1, :] = self.bc[3](self.current_x)
+        # self.u[:, 0] = self.bc[0](self.current_y)
+        # self.u[:, -1] = self.bc[1](self.current_y)
 
     def iterative_solver(self, num_times=20):
         ''' Execute the interative solver to improve the solution u
@@ -99,6 +130,7 @@ class MultiGrid2D:
                     self.u[i, j] = 0.25 * (self.u[i+1, j] + self.u[i-1, j] + self.u[i, j-1] + self.u[i, j+1]) - self.f(self.current_x[i], self.current_y[j]) * dx**2 / 4.0
             self.counter += 1
             self.error.append(self.get_error())
+            # self.set_boundary()
 
     def plot_error(self):
         ''' plot the error agains the interation count.'''
@@ -107,18 +139,18 @@ class MultiGrid2D:
         axes = fig.add_subplot(1, 1, 1)
         axes.plot(self.error)
 
-    def plot(self, u_true, u_test):
+    def plot(self, u_true):
         ''' Plot u(x) '''
         fig = plt.figure()
         fig.set_figwidth(fig.get_figwidth())
-        axes = fig.add_subplot(2, 3, 1)
+        axes = fig.add_subplot(1, 3, 1)
         plot = axes.pcolor(self.current_x, self.current_y, self.u, cmap=plt.get_cmap("Blues"))
         fig.colorbar(plot, label="$U$")
         axes.set_title("Computed Solution - MG")
         axes.set_xlabel("x")
         axes.set_ylabel("y")
 
-        axes = fig.add_subplot(2, 3, 2)
+        axes = fig.add_subplot(1, 3, 2)
         X, Y = np.meshgrid(self.current_x, self.current_y)
         plot = axes.pcolor(self.current_x, self.current_y, u_true(X, Y), cmap=plt.get_cmap("Blues"))
         fig.colorbar(plot, label="$U$")
@@ -126,7 +158,7 @@ class MultiGrid2D:
         axes.set_xlabel("x")
         axes.set_ylabel("y")
 
-        axes = fig.add_subplot(2, 3, 3)
+        axes = fig.add_subplot(1, 3, 3)
         X, Y = np.meshgrid(self.current_x, self.current_y)
         plot = axes.pcolor(self.current_x, self.current_y, abs(u_true(X, Y)-self.u), cmap=plt.get_cmap("Blues"))
         fig.colorbar(plot, label="$U$")
@@ -134,49 +166,51 @@ class MultiGrid2D:
         axes.set_xlabel("x")
         axes.set_ylabel("y")
 
-        fig.set_figwidth(fig.get_figwidth())
-        axes = fig.add_subplot(2, 3, 4)
-        plot = axes.pcolor(self.current_x, self.current_y, u_test, cmap=plt.get_cmap("Blues"))
-        fig.colorbar(plot, label="$U$")
-        axes.set_title("Computed Solution")
-        axes.set_xlabel("x")
-        axes.set_ylabel("y")
+        # fig.set_figwidth(fig.get_figwidth())
+        # axes = fig.add_subplot(2, 3, 4)
+        # plot = axes.pcolor(self.current_x, self.current_y, u_test, cmap=plt.get_cmap("Blues"))
+        # fig.colorbar(plot, label="$U$")
+        # axes.set_title("Computed Solution")
+        # axes.set_xlabel("x")
+        # axes.set_ylabel("y")
 
-        axes = fig.add_subplot(2, 3, 5)
-        X, Y = np.meshgrid(self.current_x, self.current_y)
-        plot = axes.pcolor(self.current_x, self.current_y, u_true(X, Y), cmap=plt.get_cmap("Blues"))
-        fig.colorbar(plot, label="$U$")
-        axes.set_title("True Solution")
-        axes.set_xlabel("x")
-        axes.set_ylabel("y")
+        # axes = fig.add_subplot(2, 3, 5)
+        # X, Y = np.meshgrid(self.current_x, self.current_y)
+        # plot = axes.pcolor(self.current_x, self.current_y, u_true(X, Y), cmap=plt.get_cmap("Blues"))
+        # fig.colorbar(plot, label="$U$")
+        # axes.set_title("True Solution")
+        # axes.set_xlabel("x")
+        # axes.set_ylabel("y")
 
-        axes = fig.add_subplot(2, 3, 6)
-        X, Y = np.meshgrid(self.current_x, self.current_y)
-        plot = axes.pcolor(self.current_x, self.current_y, abs(u_true(X, Y)-u_test), cmap=plt.get_cmap("Blues"))
-        fig.colorbar(plot, label="$U$")
-        axes.set_title("Error")
-        axes.set_xlabel("x")
-        axes.set_ylabel("y")
+        # axes = fig.add_subplot(2, 3, 6)
+        # X, Y = np.meshgrid(self.current_x, self.current_y)
+        # plot = axes.pcolor(self.current_x, self.current_y, abs(u_true(X, Y)-u_test), cmap=plt.get_cmap("Blues"))
+        # fig.colorbar(plot, label="$U$")
+        # axes.set_title("Error")
+        # axes.set_xlabel("x")
+        # axes.set_ylabel("y")
 
-
-        plt.show()
+        plt.savefig("%s.pdf" % self.name)
+        #plt.show()
 
     def get_error(self):
         X, Y = np.meshgrid(self.current_x, self.current_y)
 
         return np.linalg.norm(self.u-self.u_true(X,Y), ord=np.infty)
 
-    def v_sched(self, num_down=3, num_up=3, u_true=None):
+    def v_sched(self, num_down=2, num_up=2, u_true=None):
         def print_error():
             if u_true != None:
                 print 'Error with mg', self.get_error(u_true)
 
-        self.iterative_solver()
+        # self.iterative_solver() 
+        self.step()
         print_error()
 
         for i in xrange(num_down):
             self.restrict()
-            self.iterative_solver()
+            # self.iterative_solver() 
+            self.step()
             print_error()
 
             # solve linear system exactly
@@ -196,8 +230,34 @@ class MultiGrid2D:
 
         for j in xrange(num_up):
             self.interpolate()
-            self.iterative_solver()
+            # self.iterative_solver() 
+            self.step()
             print_error()
+
+    def step(self, pre=1, post=1):
+        ''' one level of relaxation on u'''
+        X, Y = np.meshgrid(self.current_x, self.current_y)
+        
+        for ii in xrange(pre): self.iterative_solver()
+        
+        d = self.f(X, Y) - np.dot(self.A_list[self.level-1], self.u.reshape(len(self.u)**2)).reshape((len(self.u), len(self.u)))
+        R = get_R(len(self.current_x[::2]), 2)
+        d = np.dot(R, d.reshape(len(d)**2))
+        # d = d.reshape(int(np.sqrt(len(d))), int(np.sqrt(len(d))))
+
+        # Solve Av = d
+        # print self.A_list[self.level].shape
+        # print d.reshape(len(d)**2).shape
+        v = np.linalg.solve(self.A_list[self.level], d)
+        T = get_T(len(self.current_x[::2]), 2)
+        v = np.dot(T, v)
+        v = v.reshape(int(np.sqrt(len(v))), int(np.sqrt(len(v))))
+
+        # improve u
+        self.u = self.u + v
+        self.set_boundary()
+
+        for jj in xrange(post): self.iterative_solver()
 
 
     def test(self):
